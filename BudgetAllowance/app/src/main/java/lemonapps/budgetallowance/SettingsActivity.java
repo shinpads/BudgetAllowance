daabe37b29ff595c6fc29ad27493c43d8dd5fb0d
marshmelllo <robfarlow@gmail.com>
2016-12-13 18:26:42 -0500

    Setup project

---
+++ BudgetAllowance/app/src/main/java/lemonapps/budgetallowance/SettingsActivity.java
@@ -0,0 +1,11 @@
+package lemonapps.budgetallowance;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rob on 12/13/2016.
+ */
+public class SettingsActivity extends AppCompatActivity{
+
+
+}
