6ab638d2c16ebef82ee24859dba3d5b933ff1f90
marshmelllo <robfarlow@gmail.com>
2017-01-07 22:47:12 -0500

    setup allowance time spinner

--- BudgetAllowance/app/src/main/java/lemonapps/budgetallowance/addAllowance.java
+++ BudgetAllowance/app/src/main/java/lemonapps/budgetallowance/addAllowance.java
@@ -2,12 +2,15 @@ package lemonapps.budgetallowance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class addAllowance extends AppCompatActivity {
-
+    Spinner time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_allowance);
+        time = (Spinner)findViewById(R.id.timeSpinner);
+
     }
 }
